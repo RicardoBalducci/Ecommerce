37e2103cab5aa3df99220120b3dc714abfe22ec5
Ricardo Balducci <Bandrock2806@gmail.com>
2024-04-14 11:17:44 -0400

    Update

--- apps/web-products/src/App.tsx
+++ apps/web-products/src/App.tsx
@@ -13,35 +13,3 @@ function App() {
 }
 
 export default App;
-
-/*
-<Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-
-/**import Principal from "./module/principal/principal";
-import { Prueba } from "./prueba";
- * 
- * npm install react-router-dom
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-
-function App() {
-  return (
-    <Router>
-      {}
-      </Router>
-    );
-  }
-  function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        { Agrega más rutas según tus necesidades }
-        </Switch>
-        </Router>
-      );
-    }
- */
