0661a611c76a0063a0194e991a491d473f29be2b
Ricardo Balducci <Bandrock2806@gmail.com>
2024-05-30 17:47:52 -0400

    Update

--- apps/api-products/src/app.module.ts
+++ apps/api-products/src/app.module.ts
@@ -5,8 +5,32 @@ import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
 import { APP_FILTER } from "@nestjs/core";
 import { NotFoundExceptionFilter } from "./not-found.filter";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: process.env.POSTGRES_SSL === "true",
+      extra: {
+        ssl:
+          process.env.POSTGRES_SSL === "true"
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      },
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, "../../", "web-products/dist"),
       exclude: ["api-products/*"],
